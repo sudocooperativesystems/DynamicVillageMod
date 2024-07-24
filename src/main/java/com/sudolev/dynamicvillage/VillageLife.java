9550fdcdb91bf84c96f88dfb0e8875fee8d5b674
MadDog <a0903932792@gmail.com>
2024-07-25 02:26:34 +0800

    Update to 1.20.1

--- src/main/java/com/sudolev/dynamicvillage/VillageLife.java
+++ src/main/java/com/sudolev/dynamicvillage/VillageLife.java
@@ -13,36 +13,33 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(VillageLife.MODID)
-public class VillageLife
-{
+public class VillageLife {
     public static final String MODID = "dynamicvillage";
 
-    public VillageLife()
-    {
+    public VillageLife() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
 
         ModVillagers.register(modEventBus);
 
-        MinecraftForge.EVENT_BUS.register(this);    }
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(ModVillagers::registerPOIs);
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
         }
     }
 }
